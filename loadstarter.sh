cb41f440317b02bffaf0c2fff7bb4a7b99e85d2f
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2016-07-14 20:01:41 +0000

    improve usage for starter script

--- loadstarter.sh
+++ loadstarter.sh
@@ -1,5 +1,49 @@
 #!/usr/bin/env bash
 
+declare -r progname="$(basename "${0}")"
+
+function usage
+{
+    cat <<EOF
+
+$(printf "${BOLD}NAME${NORM}")
+  ${progname} - starts oltpbench docker containers.
+
+$(printf "${BOLD}DESCRIPTION${NORM}")
+  Requires all the arguments necessary to create and run the oltpbench docker
+  container. This will generate a config on the fly and start the container in
+  restart unless stopped mode, with benchmark set to run for 24 hours at a time.
+
+$(printf "${BOLD}SYNOPSIS${NORM}")
+  ${progname} [ options ] ...
+
+$(printf "${BOLD}OPTIONS${NORM}")
+  -u <username>
+    username of database user that will run the benchmark.
+  -p <password>
+    password of the database user.
+  -t <type>
+    type of database you are connecting to.
+  -d <name>
+    name of the database you connecting to.
+  -n <port>
+    port number the database is listening on.
+  -f <fqdn>
+    FQDN of the database you are connecting to.
+  -b <bench>
+    type of benchmark you wish to run.
+
+$(printf "${BOLD}IMPLEMENTATION${NORM}")
+  version         ${progname} (2016-07-14)
+  author          Ritchie J Latimore <rijalati@gmail.com>
+  copyright       Copyright (c) 2016 Ritchie J Latimore
+
+EOF
+
+    exit 2
+}
+
+(( $# == 0 )) && usage
 
 while getopts :u:p:t:d:n:f:b: OPT; do
     case $OPT in
@@ -26,11 +70,13 @@ while getopts :u:p:t:d:n:f:b: OPT; do
             ;;
         *)
             printf "\nOption -${BOLD}%s${NORM} not allowed.\n\n" "${OPTARG}"
+	    usage
             ;;
     esac
 done
 
-docker run -ti --restart unless-stopped -e DBUSER="${DBUSER}" -e DBPASS="${DBPASS}" -e DBTYPE="${DBTYPE}" \
-    -e DBNAME="${DBNAME}" -e DBPORT="${DBPORT}" -e DBFQDN="${DBFQDN}" -e BENCH="${BENCH}" \
-    --entrypoint /start.sh --name ${DBFQDN} rijalati/oltpbench
+docker run -d --restart unless-stopped -e DBUSER="${DBUSER}" \
+       -e DBPASS="${DBPASS}" -e DBTYPE="${DBTYPE}" -e DBNAME="${DBNAME}" \
+       -e DBPORT="${DBPORT}" -e DBFQDN="${DBFQDN}" -e BENCH="${BENCH}" \
+       --entrypoint /start.sh --name ${DBFQDN} rijalati/oltpbench
 
