870acf49a3a7d5b45c0775cd1a2ba7ca3d8dede4
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2017-05-01 14:44:42 +0000

    update gitignore

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,16 @@ MAINTAINER rijalati@gmail.com
 RUN apk update --no-cache \
     && apk add git tar mksh && git clone https://github.com/rijalati/oltpbench.git
 WORKDIR /oltpbench
-
-RUN cd /oltpbench && git checkout errorprone && ant clean && ant
-RUN mkdir /oltpbench/templates /config-templates
-COPY my-templates/ /oltpbench/templates
+COPY my-templates/ /oltpbench/templates/
 COPY config-templates/ /oltpbench/config-templates/
 COPY start.sh /start.sh
 COPY bench-confgen.sh bench-confgen.sh
-COPY ojdbc-get.sh ojdbc-get.sh
 COPY ntlmauth.dll /oltpbench/lib/
-
+COPY ojdbc-get.sh ojdbc-get.sh
 RUN chmod +x oltpbenchmark ojdbc-get.sh /start.sh
+RUN ./ojdbc-get.sh
+RUN git checkout errorprone && ant clean && ant
+
 
 ENV DBFQDN='' DBUSER='' DBPASS='' DBTYPE='' DBNAME='' DBPORT='' BENCH=''
 ENV CLEARBOOL='' CREATEBOOL='' EXECBOOL='' LOADBOOL=''
--- ojdbc-get.sh
+++ ojdbc-get.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env mksh
-
-#URL="http://download.oracle.com/otn/utilities_drivers/jdbc/122010/ojdbc8.jar"
+set -vex
+URL="https://download.oracle.com/otn-pub/utilities_drivers/jdbc/122010/ojdbc8.jar"
 #URL="http://download.oracle.com/otn/utilities_drivers/jdbc/121020/ojdbc7.jar"
-URL="http://download.oracle.com/otn/utilities_drivers/jdbc/11204/ojdbc6.jar"
-cd lib || exit 2
+#URL="http://download.oracle.com/otn/utilities_drivers/jdbc/11204/ojdbc6.jar"
 
 print 'By downloading the Oracle Database JDBC Driver you are
 accepting the OTN License Agreement.
@@ -11,8 +10,9 @@ accepting the OTN License Agreement.
 This is a link to a copy of the OTN License Agreement:
 <http://www.oracle.com/technetwork/licenses/distribution-license-152002.html>'
 
-curl --location --remote-name \
-     --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+curl --location-trusted --remote-name -# \
+     --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
      --insecure "${URL}"
-
-cd ..
+wget -c -O ojdbc8.jar --no-check-certificate --no-cookies --header \
+     "Cookie: oraclelicense=accept-securebackup-cookie" "${URL}"
+#mv -v ojdbc[6-8].jar /oltpbench/lib/
