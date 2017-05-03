11c9c9cee970374ba67a84186f32a052d122d581
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2017-05-03 22:50:40 +0000

    don't export dialects (fails)

--- start.sh
+++ start.sh
@@ -16,6 +16,6 @@ fi
                          --clear "${CLEARBOOL:-true}" \
                          --create "${CREATEBOOL:-true}" \
                          --execute "${EXECBOOL:-true}" \
-                         --load ${LOADBOOL:-true} --dialects-export true
+                         --load ${LOADBOOL:-true}
 
 exit $?
