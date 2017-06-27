e462bc19a4949ce9e8a33d1ac13c702c11750245
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2017-06-27 00:59:59 +0000

    escape mvn var

--- db2jcc4-get.sh
+++ db2jcc4-get.sh
@@ -8,7 +8,7 @@ function download_driver
          s3auth.bm-engops.com/oltpbench/db2jcc4.jar > lib/db2jcc4.jar \
         || printf "Downloading DB2 jdbc driver failed, please provide the basic
 auth base64 encoded creds as the environment var BASICAUTH (the correct value
-is in Keeper). If you are outside Blue Medora consider building your own image
+is in Keeper). If you do not work for Blue Medora consider building your own image
 and manually embedding the driver in the image, or check out s3auth.com if you
 would like to put it on s3 like we did.\n"
 
@@ -23,7 +23,7 @@ function update_pom
     <artifactId>db2jcc4</artifactId>
     <version>4.23.42</version>
     <scope>system</scope>
-    <systemPath>${basedir}/lib/db2jcc4.jar</systemPath>
+    <systemPath>\${project.basedir}/lib/db2jcc4.jar</systemPath>
 </dependency>
 EOF
 )"
