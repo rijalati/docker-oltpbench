b087537ae30c3e85858ba1975e98e3f3fd074307
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2018-06-21 15:12:48 +0000

    add 5min sleep for failure

--- start.sh
+++ start.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env mksh
 
+
 set -vex
 
-printf '%s\n' "${ENV}"
+echo "${ENV}"
 
 export target="/oltpbench/templates"
 
-if [[ -z ${DBUSER} && -z ${DBPASS} && -z ${DBTYPE} && -z ${DBNAME} && -z ${DBPORT} ]]; then
-    echo "Skipping config generation...\n"
-else
-    /oltpbench/bench-confgen.sh -f "${DBFQDN}" -u "${DBUSER}" \
-        -p "${DBPASS}" -t "${DBTYPE}" -d "${DBNAME}" \
-        -n "${DBPORT}" -b "${BENCH}" -o "${target}" \
-        -r "${RATE}" -c "${CLIENTS}" -s "${SCALE}" -i "${ISOLATION}"
-fi
+/oltpbench/bench-confgen.sh -f "${DBFQDN}" -u "${DBUSER}" \
+    -p "${DBPASS}" -t "${DBTYPE}" -d "${DBNAME}" \
+    -n "${DBPORT}" -b "${BENCH}" -o "${target}" \
+    -r "${RATE}" -c "${CLIENTS}" -s "${SCALE}" -i "${ISOLATION}"
+
 
 /oltpbench/oltpbenchmark -b "${BENCH}" -c "${target}/${DBFQDN}.xml" \
                          --clear "${CLEARBOOL:-true}" \
                          --create "${CREATEBOOL:-true}" \
                          --execute "${EXECBOOL:-true}" \
-                         --load "${LOADBOOL:-true}"
+                         --load "${LOADBOOL:-true}" \
+			 || sleep 300
 
 exit $?
